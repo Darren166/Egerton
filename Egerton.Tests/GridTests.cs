408bb5205573c674f05fb20cde5c298cb3942246
Darren166 <darren.hall@cognim.co.uk>
2015-09-24 18:02:11 +0100

    Added Given When Then

--- Egerton.Tests/GridTests.cs
+++ Egerton.Tests/GridTests.cs
@@ -7,21 +7,31 @@ namespace Egerton.Tests
 	[TestFixture]
 	public class GridTests
 	{
-		[TestCase(1, 1)]
-		[TestCase(3, 9)]
-		[TestCase(7, 49)]
-		[TestCase(10, 100)]
-		public void New_Grid_Returns_Correct_Number_Of_Cells(int size, int expectedNumberOfCells)
+		[TestCase(1)]
+		[TestCase(3)]
+		[TestCase(7)]
+		[TestCase(10)]
+		public void New_Grid_Returns_Correct_Number_Of_Cells(int size)
 		{
+			// Given a size
+			// When a new grid is created
 			ProductGrid productGrid = new ProductGrid(size);
+
+			// Then the number of cells should be the square of the size
+			int expectedNumberOfCells = size * size;
 			Assert.AreEqual(productGrid.Cells.Length, expectedNumberOfCells);
 		}
 
-		[TestCase (1)]
+		[TestCase(1)]
 		[TestCase(57)]
 		[TestCase(1000)]
-		public void New_Grid_Has_Correct_Number_Of_Rows(int size) {
+		public void New_Grid_Has_Correct_Number_Of_Rows(int size)
+		{
+			// Given a size
+			// When a new grid is created
 			ProductGrid productGrid = new ProductGrid(size);
+			
+			// Then the number of rows should equal the size
 			Assert.AreEqual(productGrid.Cells.GetLength(0), size);
 		}
 
