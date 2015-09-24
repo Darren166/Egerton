91059d1a4b0742f6cf63ef23880ea3ce6929cbbf
Darren166 <darren.hall@cognim.co.uk>
2015-09-24 18:52:46 +0100

    Added basic test for generating primes

---
+++ Egerton.Tests/PrimeTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Egerton.GridSupport;
+
+namespace Egerton.Tests
+{
+	[TestClass]
+	public class PrimeTests
+	{
+		[TestMethod]
+		public void Generating_A_Sequence_Of_Primes_Returns_Correctly()
+		{
+			int[] primes = Primes.GenerateSequence(5);
+
+			CollectionAssert.AreEqual(new int[] { 2, 3, 5, 7, 11 }, primes);
+		}
+	}
+}
---
+++ Egerton/GridSupport/Primes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egerton.GridSupport
+{
+	public class Primes
+	{
+		public static int[] GenerateSequence(int size) {
+			return new int[] { 2, 3, 5, 7, 11 };
+		}
+	}
+}
