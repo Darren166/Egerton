fddef7d6baa65c84c3e84a4ff45c856db17f0083
Darren166 <darren.hall@cognim.co.uk>
2015-09-24 18:57:53 +0100

    Added testcases to cause prime generation to fail

--- Egerton.Tests/PrimeTests.cs
+++ Egerton.Tests/PrimeTests.cs
@@ -1,18 +1,21 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Egerton.GridSupport;
+using NUnit.Framework;
 
 namespace Egerton.Tests
 {
-	[TestClass]
+	[TestFixture]
 	public class PrimeTests
 	{
-		[TestMethod]
-		public void Generating_A_Sequence_Of_Primes_Returns_Correctly()
+		[TestCase(1, new int[] { 2 })]
+		[TestCase(5, new int[] { 2, 3, 5, 7, 11 })]
+		[TestCase(11, new int[] { 2, 3, 5, 7, 11 })]
+		[TestCase(27, new int[] { 2, 3, 5, 7, 11 })]
+		public void Generating_A_Sequence_Of_5_Primes_Returns_Correctly(int size, int[] expectedPrimes)
 		{
-			int[] primes = Primes.GenerateSequence(5);
+			int[] actuaPrimes = Primes.GenerateSequence(size);
 
-			CollectionAssert.AreEqual(new int[] { 2, 3, 5, 7, 11 }, primes);
+			CollectionAssert.AreEqual(expectedPrimes, actuaPrimes);
 		}
 	}
 }
