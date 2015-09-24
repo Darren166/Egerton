cb602bd1db69cedd97f550f9ac80117bcf4d8c49
Darren166 <darren.hall@cognim.co.uk>
2015-09-24 19:59:38 +0100

    Extended display to allow user selection of grid size

--- Egerton/Program.cs
+++ Egerton/Program.cs
@@ -11,16 +11,24 @@ namespace Egerton
 	{
 		static void Main(string[] args)
 		{
-			var productGrid = new ProductGrid(5);
-			for (int row = 0; row < productGrid.Cells.GetLength(0); row++)
+			int size = 1;
+			while (size != 0)
 			{
-				for (int column = 0; column < productGrid.Cells.GetLength(1); column++)
-				{
-					Console.Write(productGrid.Cells[row, column].ToString().PadLeft(5));
-				}
 				Console.WriteLine();
+				Console.WriteLine("Type a size for the grid and press return, 0 or non number to exit");
+				int.TryParse(Console.ReadLine(), out size);
+				Console.WriteLine();
+	
+				var productGrid = new ProductGrid(size);
+				for (int row = 0; row < productGrid.Cells.GetLength(0); row++)
+				{
+					for (int column = 0; column < productGrid.Cells.GetLength(1); column++)
+					{
+						Console.Write(productGrid.Cells[row, column].ToString().PadLeft(8));
+					}
+					Console.WriteLine();
+				}
 			}
-			Console.ReadKey();
 		}
 	}
 }
