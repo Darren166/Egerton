953af98e1f2cae9a11cbc569414e9afa3a0b6e16
Darren166 <darren.hall@cognim.co.uk>
2015-09-24 19:22:32 +0100

    Altered first column test to use testcases

--- Egerton.Tests/GridTests.cs
+++ Egerton.Tests/GridTests.cs
@@ -50,18 +50,19 @@ namespace Egerton.Tests
 			Assert.AreEqual(actualValues, expectedPrimes);
 		}
 
-		[Test]
-		public void First_Column_In_Grid_Contains_Primes()
+		[TestCase(1, new int[] { 2 })]
+		[TestCase(15, new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 })]
+		[TestCase(20, new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71 })]
+		public void First_Column_In_Grid_Contains_Primes(int size, int[] expectedPrimes)
 		{
-			// Given a grid of size 5
-			int[] primes = { 2, 3, 5, 7, 11 };
-			ProductGrid productGrid = new ProductGrid(5);
+			// Given a grid
+			ProductGrid productGrid = new ProductGrid(size);
 
 			// When the first row is extracted
-			int[] actualValues = Enumerable.Range(0, 5).Select(index => productGrid.Cells[index, 0]).ToArray();
+			int[] actualValues = Enumerable.Range(0, size).Select(index => productGrid.Cells[index, 0]).ToArray();
 
 			// Then they should be sequential primes
-			Assert.AreEqual(actualValues, primes);
+			Assert.AreEqual(actualValues, expectedPrimes);
 		}
 
 	}
